421c0e1db48240c0d220c46f24615d39995c0085
kangseok0102 <58051320+kangseok0102@users.noreply.github.com>
2020-12-21 22:44:28 -0500

    Update cartoon.py

--- Web Scraping Project/cartoon.py
+++ Web Scraping Project/cartoon.py
@@ -11,10 +11,13 @@ soup = BeautifulSoup(res.text, "lxml")
 def cartoon_title_link():
     cartoons = soup.find_all("td", attrs={"class":"title"})
     rates = soup.find_all("div", attrs={"class":"rating_type"})
+    total_rates = 0
     for line in range(len(cartoons)):
         cartoons_list = cartoons[line].a.get_text()
         link_list = cartoons[line].a["href"]
         rate_list = rates[line].find("strong").get_text()
+        total_rates += float(rate_list)
         print("웹툰정보:", cartoons_list,'\n', "Link:""https://comic.naver.com/" + link_list, '\n',
               "평점:", rate_list,"점")
+    print("평균 평점:", round((total_rates / len(cartoons)),2))
 cartoon_title_link()
